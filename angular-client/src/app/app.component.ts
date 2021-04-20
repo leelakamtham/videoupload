302edc6817e28bb44a4e428fbdd8cadc7cb8bb3d
Leelavathi <leelakamtham@gmail.com>
2021-04-20 12:01:10 +0200

    added frontend dependencies

---
+++ angular-client/src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component ,OnInit} from '@angular/core';
+import {HttpClientModule , HttpClient}     from  '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class  AppComponent implements OnInit {
+
+  uploadedFiles: Array < File > ;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  fileChange(element) {
+    this.uploadedFiles = element.target.files;
+}
+
+
+
+
+
+
+upload() {
+  let formData = new FormData();
+  for (var i = 0; i < this.uploadedFiles.length; i++) {
+      formData.append("upload[]", this.uploadedFiles[i], this.uploadedFiles[i].name);
+  }
+  this.http.post('/api/upload', formData)
+  .subscribe((response) => {
+       console.log('response received is ', response);
+  })
+}
+
+/*
+getvideos(){
+  let video = [];
+  for(var i =0; i < this.uploadedFiles.length;i++){
+     video+=
+  }
+  this.http.get('/api',)
+  .subscribe((response)=>
+  console.log('response received is',response));
+}
+*/
+
+
+
+
+
+
+
+
+}
+
+
